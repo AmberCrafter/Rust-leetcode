ee94060182a7fe8457f200480415662a1cb0027b
AmberCrafter <flamingm321@gmail.com>
2022-03-28 16:11:52 +0000

    leetcode no49.Group Anagrams

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,1 +1,2 @@
 pub mod no48_rotate_image;
+pub mod no49_group_anagrams;
---
+++ src/problems/no49_group_anagrams.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<String, Vec<String>> = HashMap::new();
+        for s in strs {
+            let mut arr: Vec<char> = s.chars().collect();
+            arr.sort();
+            let key: String = arr.into_iter().collect();
+            map.entry(key)
+                .and_modify(|x| x.push(s.clone()))
+                .or_insert(vec![s]);
+        }
+
+        map
+            .into_iter()
+            .map(|(_, value)| value)
+            .collect::<Vec<Vec<String>>>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        let inputs = vec!["abc".to_string(), "edf".to_string(), "bac".to_string()];
+        let expected = vec![["abc".to_string(),"bac".to_string()].to_vec(), ["edf".to_string()].to_vec()];
+        let result = Solution::group_anagrams(inputs);
+        assert_eq!(expected,result);
+    }
+}
