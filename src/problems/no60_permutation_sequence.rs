c4084304046138afc5daaa4c56c653b39af8b66d
AmberCrafter <flamingm321@gmail.com>
2022-04-08 18:22:31 +0000

    improve no.60. ref. https://www.cnblogs.com/grandyang/p/4358678.html

--- src/problems/no60_permutation_sequence.rs
+++ src/problems/no60_permutation_sequence.rs
@@ -1,71 +1,69 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn permute_one<T>(nums: &Vec<T>) -> Vec<T> 
-    where 
-        T: PartialEq + PartialOrd + Copy + std::fmt::Debug,
-    {
-        /*
-        example: 
-                        nums = [2 ,1, 6, 4, 2]
-        step 1: find the decrease point from the end of list
-                                   ^
-                                   dp
-           
-        step 2: swap the dp value with the larger one which figure out from the end of list
-                                            ^
-                                            swap target
-        step 3: reverse the part of the list after dp
-                [2, 2, 6, 4, 1]  -> [2, 1, 4, 6, 2]
-                       ^
-                       reverse  start point
-        */
+    // ref. https://www.cnblogs.com/grandyang/p/4358678.html
+    /* note. 
+    ex.
+    n = 5
+    k = 6
+    arr = [1,2,3,4,5]
+    res = [_,_,_,_,_]
+    
+    ----------------------------------------------------------------------------------------------------
+    由於此permutation method 會優先調整陣列尾部的數字，因此前頭的數字會存在連續出現的次數，可由排列組合判定關係
+    [1,_,_,_,_]
+     ^
+    第一位fix的次數為，
+        第二位有 4 種可能
+        第三位有 3 種可能
+        第四位有 2 種可能
+        第五位有 1 種可能
+    因此第一位的重複次數為24次
 
+    [n,2,_,_,_]
+    第二位fix的次數為，
+        第三位有 3 種可能
+        第四位有 2 種可能
+        第五位有 1 種可能
+    因此第二位的重複次數為6次
 
-        let nums_len = nums.len();
-        
-        let mut nums = (*nums).clone();
-        if nums_len==0 {return nums;}
-        // println!("Input: {:?}", nums);
+    [n,n,3,_,_]
+    第三位fix的次數為，
+        第四位有 2 種可能
+        第五位有 1 種可能
+    因此第三位的重複次數為2次
 
-        // step 1: find the decrease point
-        let mut dp = 0;
-        let mut flag = true;
-        for i in 1..nums_len{
-            // println!("Step1: i:{:?}\tx:{:?}\ty:{:?}", i, nums[nums_len-i], nums[nums_len-i-1]);
-            if nums[nums_len-i]>nums[nums_len-i-1] {
-                dp = nums_len-i-1;
-                flag = false;
-                break;
-            }
-        };
+    [n,n,n,4,_]
+    第四位fix的次數為，
+        第五位有 1 種可能
+    因此第四位的重複次數為1次
 
-        // println!("Step1: {:?}\t {:?}", dp, nums);
-        if dp==0 && flag{
-            nums.reverse(); return nums
-        };
+    [n,n,n,n,5]
+    第五位的重複次數為1次
 
-        // step 2: swap the value which larger than dp
-        for i in 1..nums_len-dp {
-            if nums[nums_len-i]>nums[dp] {nums.swap(nums_len-i, dp); break;}
-        }
-        // println!("Step2: {:?}", nums);
+    該fix位所填入的值，可以透過殘餘的陣列元素[a,b,c,d]進行判斷，例如
+    該位重複次數為6次，選取第17位排序結果，
+    a 排序 6 次，共  6 次
+    b 排序 6 次，共 12 次
+    c 排序 5 次，共 17 次
+    故可得知填入值為c
+    
 
-        // println!("dp: {}", dp);
-        // step 3: reverse the last parts of the list after dp
-        for i in 1..=(nums_len-dp)/2 {
-            nums.swap(nums_len-i, dp+i);
-            // println!("Step3: {:?}", nums);
-        }
-        nums
-    }
+    以此循環往覆，可以判斷在特定排列序號下，各個欄位的數值。
+    */
 
     pub fn get_permutation(n: i32, k: i32) -> String {
-        let mut res = (1..=n).map(|v| v).collect::<Vec<i32>>();
-        for _ in 1..k {
-            res = Solution::permute_one(&res);
+        let mut v: Vec<char> = (0..n as u8).map(|i| (b'1' + i) as char).collect();
+        let mut k: i32 = k - 1;
+        let mut m: i32 = (1..n).product();
+        let mut answer: String = String::with_capacity(n as usize);
+        for i in 0..n - 1 {
+            answer.push(v.remove((k / m) as usize));
+            k %= m;
+            m /= n - i - 1;
         }
-        res.into_iter().map(|v| v.to_string()).collect::<String>()
+        answer.push(v[0]);
+        answer
     }
 }
 
