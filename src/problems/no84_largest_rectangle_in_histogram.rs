d3f93f9d50a4be88e89f36f513b102d407c34a68
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-07-12 02:57:19 +0000

    finishe no84 but Memory limit exceeded

--- src/problems/no84_largest_rectangle_in_histogram.rs
+++ src/problems/no84_largest_rectangle_in_histogram.rs
@@ -1,31 +1,18 @@
 pub struct Solution {}
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        // dp: use two table
-        // 1st table figure out maxiumn valied height in range [i, j]
-        // 2ed table record maxiumn area and output the last value
+        // dp
+        // table record maxiumn area and output the last value
 
         let lenght = heights.len() + 1;
 
-        let mut height_table = vec![vec![0; lenght]; lenght];
         let mut area_table = vec![vec![0; lenght]; lenght];
-
-        for i in 1..lenght {
-            height_table[i][i] = heights[i - 1];
-            height_table[i][0] = heights[i - 1];
-            height_table[0][i] = heights[i - 1];
-        }
-
+        
         for r in 1..lenght {
-            for c in r + 1..lenght {
-                height_table[r][c] = heights[c - 1]
-                    .min(height_table[r][c - 1]);
-            }
-        }
-
-        for r in 1..lenght {
+            let mut h = heights[r-1];
             for c in r..lenght {
-                area_table[r][c] = (height_table[r][c] * (c - r + 1) as i32)
+                h = h.min(heights[c-1]);
+                area_table[r][c] = (h * (c - r + 1) as i32)
                     .max(area_table[r - 1][c])
                     .max(area_table[r][c - 1]);
             }
